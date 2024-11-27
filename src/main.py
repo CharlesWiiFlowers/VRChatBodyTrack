8222ebfc398031333d8cdf19867adc8f42ba6ca2
Carlos563429 <carlitosfg.5051@gmail.com>
2024-11-26 22:09:39 -0600

    [🦀] Feature: data management

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 from modules.posemark import PoseDetector as pose
 import cv2
 
-class main():
+class Data():
+    def convertLines(result) -> dict:
+        #"Left elbow": [0, (0,0,0,0)]
+        # "Nose": [0, (0,0,0,0)]
+        track:dict = {"Nose": [0, (0,0,0,0)], "Left elbow": [13, (0,0,0,0)]}
+
+        for x in track.items():
+            # print() # Example: ('Head', [0, (0, 0, 0, 0)])
+            
+            point = result.landmark[x[1][0]]
+
+            x[1][1] = ((point.x), (point.y), (point.z), (point.visibility))
+
+            return track
+
+class Main():
     # init class
     detector = pose()
 
@@ -25,8 +40,10 @@ class main():
         if cv2.waitKey(2) & 0xFF == ord('q'):
             break
 
+        Data.convertLines(pose_landmark)
+
     cap.release()
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    main()
+    Main()
